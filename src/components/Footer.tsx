4f0afea235a7849acec3cea01902ebc74e558c76
Shimesh <yaron.orb@gmail.com>
2026-06-18 21:40:30 +0300

    chore: release v2.8.166

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -118,7 +118,7 @@ export default function Footer() {
             >
 
 
-              v2.8.165
+              v2.8.166
 
 </span>
             <span className="opacity-45" dir="ltr">By Yaron Orbach</span>
