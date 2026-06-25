454fe669dd4a586d44294d5c60ed5146010b2f10
Shimesh <yaron.orb@gmail.com>
2026-06-25 15:34:59 +0300

    Bump version to 2.8.182

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -118,7 +118,7 @@ export default function Footer() {
             >
 
 
-              v2.8.181
+              v2.8.182
 
 </span>
             <span className="opacity-45" dir="ltr">By Yaron Orbach</span>
