005cf958fcf9bf84a6bee529aab4c24c8bef10dd
Shimesh <yaron.orb@gmail.com>
2026-06-27 10:51:12 +0300

    Bump app version to 2.8.188

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 
 
-const CACHE_NAME = 'tv-industry-il-v2.8.187';
+const CACHE_NAME = 'tv-industry-il-v2.8.188';
 
 const STATIC_ASSETS = [
   '/manifest.json',
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -118,7 +118,7 @@ export default function Footer() {
             >
 
 
-              v2.8.187
+              v2.8.188
 
 </span>
             <span className="opacity-45" dir="ltr">By Yaron Orbach</span>
